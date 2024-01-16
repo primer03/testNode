40b2dc6593d44c43cd09e57f1639c8a66277907a
primer03 <Panupong.person@gmail.com>
2024-01-16 17:49:25 +0700

    add

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool
-
+require('dotenv').config();
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'students',
-    password: 'Panupong23',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD
 });
 
 module.exports = pool;
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
-const port = 3000;
+const pool = require('./db');
+const port = process.env.DB_PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/api/student', async (req, res) => {
+    try {
+        const users = await pool.query('SELECT *FROM student_data');
+        res.json(users.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
